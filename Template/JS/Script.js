f92594443466ac9906a04fdb488965a229adb937
zkgalax <pxd5553@gmail.com>
2026-05-03 11:39:51 +0200

    Delete Template directory

--- Template/JS/Script.js
+++
@@ -1,9 +0,0 @@
-function lanzarConfeti() {
-    // Configuración del confeti
-    confetti({
-    particleCount: 150,
-    spread: 70,
-    origin: { y: 0.6 },
-    colors: ['#d63384', '#ff69b4', '#ffffff']
-    });
-}
